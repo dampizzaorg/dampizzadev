9f3e719e4b6fd9b71686d06f39160ba905a83703
clsantos97 <clsantos.edu@gmail.com>
2018-02-07 09:05:34 +0100

    resolve

--- DamPizzaApp/src/main/java/com/dampizza/logic/io/UserManagerInterface.java
+++ DamPizzaApp/src/main/java/com/dampizza/logic/io/UserManagerInterface.java
@@ -92,8 +92,6 @@ public interface UserManagerInterface {
     public Integer checkCredential(String username, String password);
 
     public Integer checkStatus(String username);
-    public Boolean userExist(String user);
-
     
     /**
      * Change password
